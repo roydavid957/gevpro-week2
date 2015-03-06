85345427c55deffd158ec6b837fc71748eb44f6b
S2764989 <s2764989@let-edu-145-12.let.rug.nl>
2015-03-06 09:47:17 +0100

    spontal_filter.py done

--- spontal_filter.py
+++ spontal_filter.py
@@ -12,16 +12,16 @@ def main(argv):
 	#loop to compare the start and end to
 	#the TOP and BOTTOM
     for POINT in root.findall('POINT'):
-         
+        
 		#give grand-childs a variable  
         top = float(POINT.find('TOP_HZ').text)
         bottom = float(POINT.find('BOTTOM_HZ').text)
         start = float(POINT.find('F0_START').text)
         end = float(POINT.find('F0_END').text)
 		
-        if start > top and bottom:
+        if start > top or start < bottom:
             root.remove(POINT)
-        if end > top and bottom:
+        elif end > top or end < bottom:
             root.remove(POINT)
 	
 	#write the output to a different file		
