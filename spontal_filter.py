bdf752a0a02b2a61b4610e6f5987e3985ea4772c
Roy D <r.a.david@student.rug.nl>
2015-03-06 07:16:22 +0100

    spontal_filter w4

---
+++ spontal_filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+import sys
+
+def main(argv):
+	
+	#import data
+    tree = ET.parse(argv[1])
+    root = tree.getroot()
+	
+	#loop to compare the start and end to
+	#the TOP and BOTTOM
+    for POINT in root.findall('POINT'):
+         
+		#give grand-childs a variable  
+        top = float(POINT.find('TOP_HZ').text)
+        bottom = float(POINT.find('BOTTOM_HZ').text)
+        start = float(POINT.find('F0_START').text)
+        end = float(POINT.find('F0_END').text)
+		
+        if start > top and bottom:
+            root.remove(POINT)
+        if end > top and bottom:
+            root.remove(POINT)
+	
+	#write the output to a different file		
+    tree.write(argv[2])
+
+if __name__ == "__main__":
+	main(sys.argv)
