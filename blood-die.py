efea2d30002e6cc5461b0ca6a3c2bcbeb9665af9
Roy D <r.a.david@student.rug.nl>
2015-03-09 02:34:57 +0100

    blood-die veranderd omdat ik bij de vorige programma's aldoor  een onduidelijke json error kreeg

--- blood-die.py
+++ blood-die.py
@@ -4,21 +4,31 @@ import sys
 import json
 from collections import namedtuple
 
+"""a program which finds blood and die matches of several languages"""
 def main():
-	open_file = open("blood-die.json", "r")
-	filedata = json.load(open_file)
-	
-	Language = namedtuple("Country", "name, classification, blood, die")
-	
-	resultsList = []
-	
-	for language in filedata:
-		country = language[0]
-		classification = language[1]
-		bloodWords = language[2].split(",")
-		dieWords = language[3].split(",")
-		[resultsList.append(Language(country, classification, blood, die)) for blood in bloodWords if blood in dieWords]
-	print(resultsList)
+    
+    """open file"""
+    open_file = open("blood-die.json", "r")
+    filedata = json.load(open_file)
 
+    country = namedtuple("country", "language, classifications")
+    
+    resultsList = []
+    
+    """making lists of all the blood and die words"""
+    for langList in filedata:
+        name = langList[0]
+        classification = langList[1]
+        bloodWords = langList[2].strip().split(",")
+        dieWords = langList[3].strip().split(",")
+        
+        """if bloodword matches dieword append to resultsList"""
+        for blood in bloodWords:
+            for die in dieWords:
+               if blood==die:
+                    resultsList.append(country(name, classification))
+                    
+    print([language for (language, classifications) in resultsList])
+    
 if __name__ == "__main__":
 	main()
