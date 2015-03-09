5908fd36678047f88b7fbe162375b6108a45816f
Roy D <r.a.david@student.rug.nl>
2015-03-09 01:15:13 +0100

    added blood-die.json

--- blood-die.py
+++ blood-die.py
@@ -4,27 +4,21 @@ import sys
 import json
 from collections import namedtuple
 
-"""check if bloodword is in dieword, if so return True for that word"""
-def bloodISdie(lang):
-    
-    blood = lang[2].split()
-    die = lang[3].split()
-    return [True for word in blood if word in die]
-
 def main():
-    
-    language = namedtuple("language", "name, classification")
-    
-    """open jsonfile"""
-    jsonOpen = open("blood-die.json", "r")
-    jsonFile = json.load(jsonOpen)
-    
-    """add every word to list if bloodISdie returns True for that word"""
-    resultlist = []
-    [resultlist.append(language(lang[0], lang[1])) for lang in jsonFile if bloodISdie(lang)]
-    
-    """used 2 tabs to clearify both results"""
-    [print(result.name, "		", result.classification) for result in resultlist]
-    
+	open_file = open("blood-die.json", "r")
+	filedata = json.load(open_file)
+	
+	Language = namedtuple("Country", "name, classification, blood, die")
+	
+	resultsList = []
+	
+	for language in filedata:
+		country = language[0]
+		classification = language[1]
+		bloodWords = language[2].split(",")
+		dieWords = language[3].split(",")
+		[resultsList.append(Language(country, classification, blood, die)) for blood in bloodWords if blood in dieWords]
+	print(resultsList)
+
 if __name__ == "__main__":
 	main()
